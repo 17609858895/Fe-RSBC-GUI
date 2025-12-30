de41172ae175a80577ac50e9fef9cd8c1bca8899
CL <129781090+17609858895@users.noreply.github.com>
2025-12-30 23:02:46 +1300

    Refactor App.py for improved structure and styling

--- App.py
+++ App.py
@@ -70,7 +70,7 @@ html, body, [class*="css"]{
 }
 
 .title{
-  font-size: 1.30rem;             /* ✅ 标题更大 */
+  font-size: 1.10rem;             /* ✅ 标题更大 */
   font-weight: 800;
   letter-spacing: -0.02em;
   margin: 0 0 12px 0;
@@ -278,6 +278,7 @@ if prediction is not None and df_result is not None:
         file_name=text["file_name"],
         mime="text/csv"
     )
+
 
 
 
